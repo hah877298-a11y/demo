7c9f26ce686cfbb5671720b52484be1dae8d6655
HAH877298-A11y <HAH877298@gmail.com>
2026-06-25 01:54:20 +0800

    feat: 成功实现第一个 STM32 串口打印 Hello World

---
+++ README.md
@@ -0,0 +1,13 @@
+# STM32 UART 串口打印项目 🚀
+
+这是我的第一个嵌入式开发项目！通过 STM32 的 UART1 串口成功向电脑发送了 "Hello World"。
+
+## 🛠️ 硬件环境
+- 开发板：STM32F103C8T6 (Blue Pill)
+- 烧录器：ST-Link V2
+- 串口模块：CH340 USB 转 TTL
+
+## 📈 运行结果
+按下板子上的 Reset 键后，电脑串口助手成功接收到数据：
+
+![Hello World 运行截图](./hello_world.png)
--- USER/main.c
+++ USER/main.c
@@ -1,9 +1,50 @@
-#include<stm32f10x.h>
+#include "stm32f10x.h"
+#include "stm32f10x_usart.h"   // 串口功能
+#include "stm32f10x_gpio.h"    // 引脚功能
+#include "stm32f10x_rcc.h"     // 时钟功能
+
+void USART1_SendChar(char ch)
+  {
+        while (USART_GetFlagStatus(USART1, USART_FLAG_TXE) == RESET);
+        USART_SendData(USART1, ch);
+	
+	}  
+
+	 void USART1_SendString(char *str)
+  {
+        while (*str)
+        {
+                USART1_SendChar(*str);
+                str++;
+        }
+  }
 
 int main (void )
 {
-  
+   RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE);
+   RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
+	
+   GPIO_InitTypeDef GPIO_InitStructure;
+   GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9;
+   GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+   GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+   GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+   USART_InitTypeDef USART_InitStructure;
+   USART_InitStructure.USART_BaudRate = 115200;
+   USART_InitStructure.USART_WordLength = USART_WordLength_8b;
+   USART_InitStructure.USART_StopBits = USART_StopBits_1;
+   USART_InitStructure.USART_Parity = USART_Parity_No;
+   USART_InitStructure.USART_Mode = USART_Mode_Tx;
+   USART_Init(USART1, &USART_InitStructure);
+
+   USART_Cmd(USART1, ENABLE);
+
+    USART1_SendString("Hello World\r\n");
+
+        while (1)
+        {
+        }
 	
 	
-	return 0;
 }
